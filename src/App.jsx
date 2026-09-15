5189c7e0e4f7cbfa95fbb9b4fe180622cecc3088
elthaama-ux <elthaama@gmail.com>
2026-09-15 13:30:51 +0300

    Add Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Sebhaty from './Sebhaty'
+import { Analytics } from '@vercel/analytics/react'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Sebhaty />
+      <Analytics />
     </>
   )
 }
